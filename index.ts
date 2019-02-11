a4e9aa85038109868f2607c9d2692e1f32291794
Robert Ying <yingrui205@gmail.com>
2019-02-11 22:44:12 +0800

    project: initial graphql server configuration

--- index.ts
+++ index.ts
@@ -1,22 +1,52 @@
-import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
+import * as dotenv from "dotenv";
 import * as express from "express";
+import { DIRECTIVES } from "graphql-codegen-typescript-mongodb";
+import { getDB } from "./db";
+import courseResolvers from "./resolvers/course";
+import mutationResolvers from "./resolvers/mutation";
+import queryResolvers from "./resolvers/query";
+import courseType from "./schemas/course";
+import mutationType from "./schemas/mutation";
+import queryType from "./schemas/query";
+import teacherType from "./schemas/teacher";
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
+(async () => {
+  dotenv.config();
 
-const resolvers = {
-  Query: {
-    hello: () => "Hello world!"
-  }
-};
+  const db = await getDB();
 
-const server = new ApolloServer({ typeDefs, resolvers });
-const app = express();
-server.applyMiddleware({ app });
+  const typeDefs = [
+    DIRECTIVES,
+    teacherType,
+    courseType,
+    queryType,
+    mutationType
+  ];
 
-app.listen(23333, () =>
-  console.log(`🚀  Server ready at http://localhost:23333${server.graphqlPath}`)
-);
+  const resolvers: any = {
+    ...courseResolvers,
+    ...queryResolvers,
+    ...mutationResolvers
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: {
+      db
+    },
+    engine: {
+      apiKey: process.env.ENGINE_API_KEY
+    }
+  });
+  const app = express();
+  server.applyMiddleware({ app });
+
+  app.listen(23333, () =>
+    // tslint:disable-next-line: no-console
+    console.log(
+      `🚀  Server ready at http://localhost:23333${server.graphqlPath}`
+    )
+  );
+})();
