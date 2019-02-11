2c98c527029a631d8262343d4ba4acd3d8a0d313
Robert Ying <yingrui205@gmail.com>
2019-02-11 22:43:32 +0800

    project: setup database

---
+++ db.ts
@@ -0,0 +1,16 @@
+// tslint:disable: no-console
+
+import * as mongodb from "mongodb";
+
+const url = "mongodb://localhost:27017";
+export const client = new mongodb.MongoClient(url, { useNewUrlParser: true });
+
+export async function getDB() {
+  if (client.isConnected()) {
+    return client.db("thu-course-api");
+  } else {
+    await client.connect();
+    console.log("💾  Connected to database");
+    return client.db("thu-course-api");
+  }
+}
